3aa3bdc5f0affb25e341c2a95c1dcd5c4fa0c429
hoiverduas <hoiver75@gmail.com>
2024-11-10 14:34:34 -0500

    Se crea Controlador para probrar la creacion de usuarios

---
+++ src/main/java/com/talataa/ecommerce_app/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.talataa.ecommerce_app.controller;
+
+import com.talataa.ecommerce_app.model.User;
+import com.talataa.ecommerce_app.service.imp.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(this.userService.createUser(user));
+        }catch (RuntimeException e){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<User>> findAllUser(){
+        try {
+            return ResponseEntity.
+                    status(HttpStatus.OK)
+                    .body(this.userService.findAllUser());
+        }catch (RuntimeException e){
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(this.userService.findUserById(id));
+        }catch (RuntimeException e){
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+}
